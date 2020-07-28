4c266cfc4193a1406e3ddf5087c0525a53a1b776
RajatSablok <rajat.main06@gmail.com>
2020-07-28 17:04:42 +0530

    url init

--- api/models/url.js
+++ api/models/url.js
@@ -5,6 +5,12 @@ const urlSchema = mongoose.Schema({
   longURL: { type: String },
   shortURL: { type: String },
   shortCode: { type: String },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  createdAt: { type: String },
+  numClicks: { type: Number, default: 0 },
 });
 
 module.exports = mongoose.model("URL", urlSchema);
--- api/routes/url.js
+++ api/routes/url.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const otpGenerator = require("otp-generator");
+
 const URL = require("../models/url");
+
+const checkAuth = require("../middleware/checkAuth");
 
 const router = express.Router();
 
-router.post("/", async (req, res) => {
+//generate short url from longurl
+router.post("/", checkAuth, async (req, res) => {
   const { longURL } = req.body;
+
+  const createdBy = req.user.userId;
 
   const shortCode = otpGenerator.generate(6, {
     digits: false,
@@ -15,28 +21,69 @@ router.post("/", async (req, res) => {
     alphabets: true,
   });
 
-  const shortURL = `${process.env.baseURL}/${shortCode}`;
+  const shortURL = `${process.env.baseURL}${shortCode}`;
+
+  const createdAt = new Date();
 
   const newURL = new URL({
     _id: new mongoose.Types.ObjectId(),
     longURL,
-    shortCode,
     shortURL,
+    shortCode,
+    createdBy,
+    createdAt,
   });
 
-  await newURL.save();
-
-  res.status(200).json({ shortCode, shortURL });
+  await newURL
+    .save()
+    .then(async (result) => {
+      res.status(201).json({
+        message: "Short URL created",
+        shortURL,
+      });
+    })
+    .catch((err) => {
+      res.status(400).json({
+        message: "Something went wrong",
+        error: err.toString(),
+      });
+    });
 });
 
+//
 router.get("/:code", async (req, res) => {
   const { code } = req.params;
 
-  await URL.findOne({ shortCode: code })
+  await URL.find({ shortCode: code })
     .then(async (result) => {
-      res.redirect(result.longURL);
+      if (result.length < 1) {
+        return res.status(404).json({
+          message: "URL not found",
+        });
+      } else {
+        const longURL = result[0].longURL;
+        const numClicks = result[0].numClicks + 1;
+        const urlId = result[0]._id;
+
+        await URL.updateOne({ _id: urlId }, { numClicks })
+          .exec()
+          .then(async (url) => {
+            res.redirect(longURL);
+          })
+          .catch((err) => {
+            res.status(400).json({
+              message: "Something went wrong",
+              error: err.toString(),
+            });
+          });
+      }
     })
-    .catch();
+    .catch((err) => {
+      res.status(400).json({
+        message: "Something went wrong",
+        error: err.toString(),
+      });
+    });
 });
 
 module.exports = router;
